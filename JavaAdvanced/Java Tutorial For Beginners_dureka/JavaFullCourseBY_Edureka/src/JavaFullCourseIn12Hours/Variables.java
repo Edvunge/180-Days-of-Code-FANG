2f6227d9f21466efe410bf58e758a93ff9c8ec0f
Edvaldo vunge <costaedy18@gmail.com>
2024-11-28 17:36:06 +0000

    Operators in Java

---
+++ JavaAdvanced/Java Tutorial For Beginners_dureka/JavaFullCourseBY_Edureka/src/JavaFullCourseIn12Hours/Variables.java
@@ -0,0 +1,11 @@
+package JavaFullCourseIn12Hours;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 29;
+        double height = 1.89;
+
+        System.out.println("my age is: " + age);
+        System.out.println("my height is: " + height);
+    }
+}
